284b069c9a316ed2be3acf582e0639e66c943212
Jadugar-24 <164513962+Jadugar-24@users.noreply.github.com>
2024-05-06 18:39:37 +0530

    Update start.py

--- OXYBOT/modules/start.py
+++ OXYBOT/modules/start.py
@@ -35,7 +35,7 @@ async def start(event):
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n"
         await event.client.send_file(
             event.chat_id,
-            "https://telegra.ph/file/0568faa16980206bdb501.jpg",
+            "https://telegra.ph/file/a1d601d01169707e7746d.jpg",
             caption=TEXT,
             buttons=START_BUTTON
         )
