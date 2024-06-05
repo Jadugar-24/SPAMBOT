a166580c277eac1646d9177e95858c46991cfb2b
CHALCOGEN <166417113+CHALCOGEN645@users.noreply.github.com>
2024-06-05 21:53:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["OXYBOT.modules." + plugin_name] = load
-    print("𝕁𝔸𝔻𝕌𝔾𝔸ℝ 𝗛𝗔𝗦 𝗜𝗠𝗣𝗢𝗥𝗧𝗘𝗗 " + plugin_name)
+    print("𝐉𝐀𝐃𝐔𝐆𝐀𝐑 𝗛𝗔𝗦 𝗜𝗠𝗣𝗢𝗥𝗧𝗘𝗗 " + plugin_name)
 
 
 files = glob.glob("OXYBOT/modules/*.py")
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\𝕁𝔸𝔻𝕌𝔾𝔸ℝ 𝗦𝗣𝗔𝗠 𝗕𝗢𝗧 𝗗𝗘𝗣𝗟𝗢𝗬𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬⚡\nMy Master ---> 𝕁𝔸𝔻𝕌𝔾𝔸ℝ")
+print("\𝐉𝐀𝐃𝐔𝐆𝐀𝐑 𝗦𝗣𝗔𝗠 𝗕𝗢𝗧 𝗗𝗘𝗣𝗟𝗢𝗬𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬⚡\nMy Master ---> 𝐉𝐀𝐃𝐔𝐆𝐀𝐑")
 
 
 async def main():
